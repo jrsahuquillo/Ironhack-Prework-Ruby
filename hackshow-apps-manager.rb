2c7e3839b4c0193fb8b20cee0d2e711ebace8a97
jrsahuquillo <jrsahuquillo@gmail.com>
2016-10-04 14:24:35 +0200

    Iteration two

--- hackshow-apps-manager.rb
+++ hackshow-apps-manager.rb
@@ -1,5 +1,5 @@
 # Hackshow Apps Manager
-#Iteration One
+#Iteration Two
 
 
 class App
@@ -14,11 +14,31 @@ class App
 	def description
 		puts @app_author + " has developed " + @app_name + " for " + @app_os + " and he has got " + @app_points.to_s + " points."
 	end
+
+
+	def evaluation
+		if @app_points < 0 || @app_points > 10
+			puts "Evaluation error, try to set another punctuation"
+
+		else
+			if @app_points >= 0 && @app_points <= 4
+				@app_evaluation = "a poor"
+			elsif @app_points >=5 && @app_points <= 6
+				@app_evaluation = "a good"
+			elsif @app_points >= 7 && @app_points <= 9
+				@app_evaluation = "a very cool"
+			else
+				@app_evaluation = "an awesome"
+			end
+		puts @app_author + "has developed " + @app_name +  " for " + @app_os + " and he has made " + @app_evaluation + " aplication."
+		end	 
+	
+	end	
+
 end
-
-
 
 
 app = App.new "ILoveApples", "Richard Stallman", "iOS", 8
 
 app.description
+app.evaluation
